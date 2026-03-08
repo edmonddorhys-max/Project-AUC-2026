c02c8e3072b8146db14f80c3009e42eb8a555019
Dorhys <edmonddorhys@gmail.com>
2026-03-08 08:30:20 -0400

    ajout des menbres du groupe

--- calculator-main.py
+++ calculator-main.py
@@ -9,3 +9,12 @@ if __name__ == "__main__":
     app = CalculatorControll(CalculatorModel, CalculatorView)
     app = app.view.get_root()
     app.mainloop()
+
+"""
+Les membres du groupe:
+1.EDMOND Dorhys H. Kelly
+2.Benjamin Roloussande
+3.Kelly Gauthier S.
+4.Dorval Jane Lauramene
+5.Cétoute Nerlande
+"""
